ba2fa6399faa1040b67c283f818ff1716958fa46
rhoverholt <rhoverholt@gmail.com>
2022-03-21 22:38:06 -0400

    Additional cleanup to shrink code and improve maintainability

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,6 +26,31 @@ const gameTable = document.getElementById("game-table"),
 
 const headerInputEls = [playXEl, playOEl, playBothEl, startBtnEl];
 
+const winningCombinations = [0, 1, 2].map((col) => [
+  [0, col],
+  [1, col],
+  [2, col],
+]); // all vertical combinations
+winningCombinations.push(
+  ...[0, 1, 2].map((row) => [
+    [row, 0],
+    [row, 1],
+    [row, 2],
+  ])
+); // add horizontal combinations
+winningCombinations.push(
+  [
+    [0, 0],
+    [1, 1],
+    [2, 2],
+  ],
+  [
+    [0, 2],
+    [1, 1],
+    [2, 0],
+  ]
+); // add the diagonal combinations
+
 [
   ["start-btn", processStartClick],
   ["undo", processUndoClick],
@@ -36,7 +61,7 @@ const headerInputEls = [playXEl, playOEl, playBothEl, startBtnEl];
 
 createGameTable(); // create the HTML to display the game and the cell event listeners
 
-displayOutput("Begin by choosing sides or simply making a move");
+outputMsgEl.textContent = "Begin by choosing sides or simply making a move";
 
 function createGameTable() {
   for (let row = 0; row < 3; row++) {
@@ -61,38 +86,17 @@ function createGameTable() {
     classes.forEach((item) => child.classList.add(item));
     return parent.appendChild(child); // adds the new div to the DOM
   }
-}
 
-// Verify the click is a valid move
-// Play the move.
-// Determine if the game is over.
-// If not, play the computer's move.
-function processCellClick(event) {
-  // ignore the click if it wasn't the player's turn
-  if (!isPlayerTurn || isGameOver) return;
+  function processCellClick() {
+    if (!isPlayerTurn || isGameOver || this.textContent) return; // ignore illegal clicks
 
-  let element = document.getElementById(event.target.id);
+    if (!isGameStarted && !startNewGame(false)) return; // try to start a new game as needed, return if invalid
 
-  // ignore the click if the cell has already been played in
-  if (element.textContent) {
-    console.log(
-      `Clicked cell (${element.id}) already contains '${element.textContent}'`
-    );
-    return;
-  }
+    processMove(this); // Play the move for the click!
 
-  // process the click that's in an empty cell
-
-  // Allow this to start the game if needed
-  if (!isGameStarted) {
-    if (!startNewGame(false)) return; //false parameters means start button was not pressed, false return means invalid user click.
-  }
-
-  // Play the move for the click!
-  processMove(element);
-
-  if (!isGameOver && !isPlayerBoth) {
-    playComputerMove(); // plays the move and ends via processMove()
+    if (!isGameOver && !isPlayerBoth) {
+      playComputerMove(); // plays the move and ends via processMove()
+    }
   }
 }
 
@@ -101,17 +105,15 @@ function startNewGame(wasButtonClicked) {
     'input[name="play-side"]:checked'
   ).value;
 
-  if (gameType === oM && !wasButtonClicked) return false; // the game has not been started!
+  if (gameType === oM && !wasButtonClicked) return false; // the game has not been started, O must wait on X to go first
 
-  isPlayerX = !(gameType === oM);
+  isPlayerX = !(gameType === oM); // set to true when X or Both are clicked
   isPlayerBoth = gameType === "Both";
   isGameStarted = true;
 
-  // disable the Header input, as a move has been made:
-  disableHeaderInput(true);
+  disableHeaderInput(true); // disable the Header input, as a move has been made:
 
-  // enable the undo and refresh buttons
-  undoBtnEl.disabled = false;
+  undoBtnEl.disabled = false; // enable the undo and refresh buttons
   refreshBtnEl.disabled = false;
 
   return true; // the game has been started!
@@ -120,43 +122,29 @@ function startNewGame(wasButtonClicked) {
 function processStartClick() {
   startNewGame(true);
 
-  if (playOEl.checked) {
-    playComputerMove(); // plays the turn and ends via processMove
-    return;
-  }
+  if (playOEl.checked) return playComputerMove(); // plays the turn and ends via processMove
 
-  // wait for the player to take their turn, whether they are X or both.
-  displayNextTurn(false);
-  return;
+  displayNextTurn(false); // wait for the player to take their turn, whether they are X or both.
 }
 
 function processUndoClick() {
-  undoMove();
+  undoMove(); // this is usually the computer's move that is undone, but when it was the players move it's the only one to undo.
 
-  if (isPlayerBoth) return; // only need to return the 1 move if the player is playing both Xs and Os
-
-  // As long as X hadn't just played in the 9th cell, and thus it remains X's turn after just one undo...
-  if (!(isPlayerX && isXsTurn(gameBoard))) undoMove();
+  if (
+    !isPlayerBoth || // no second move to undo when the player plays both sides!
+    !(isPlayerX && isXsTurn(gameBoard)) // this is necessary to prevent a second undo when X played in the last space (so no computer move) and then undoes that move
+  )
+    undoMove(); // now undo the player's move as the previous was the computer's.
 
   function undoMove() {
-    if (!moveStack.length) {
-      // no more moves to undo so start over
-      window.location.reload();
-      return; // Just to make it obvious.
-    }
+    if (moveStack.length < 2) return window.location.reload(); // with 0 or only 1 move to undo, just restart the game.
 
     isGameOver = false;
 
     let [row, col] = moveStack.pop();
 
-    if (!moveStack.length) {
-      // no more moves to undo so start over
-      window.location.reload();
-      return; // Just to make it obvious.
-    }
-
-    document.getElementById(`cell-${row}-${col}`).textContent = "";
-    gameBoard[row][col] = 0;
+    document.getElementById(`cell-${row}-${col}`).textContent = ""; // undo the move on the screen
+    gameBoard[row][col] = 0; // undo the move in the virtual game board
 
     displayNextTurn(false);
   }
@@ -167,100 +155,43 @@ function disableHeaderInput(isDisable) {
   playStatusEl.textContent = isDisable ? "I'm playing: " : "I'll play";
 }
 
-// Place a move in the given element
-// Give a message on screen showing who won or who's turn is next.
 function processMove(element) {
-  if (isXsTurn(gameBoard)) {
-    element.textContent = "X";
-  } else {
-    element.textContent = "O";
-  }
+  element.textContent = isXsTurn(gameBoard) ? "X" : "O";
 
-  // update the gameBoard
-  let row = element.id[5]; // 'cell-R-C'
-  let col = element.id[7];
+  let row = element.id[5]; // 'cell-R-C' R is 6th, C is 8th so 5, 7
+  let col = element.id[7]; // this array notation is usable on strings.
 
-  moveStack.push([row, col]);
-  gameBoard[row][col] = element.textContent;
+  moveStack.push([row, col]); // keep up with moves so we can undo as needed
+  gameBoard[row][col] = element.textContent; // make the move in our virtual game board
 
-  displayNextTurn(getGameStatus(gameBoard));
-  return false; // game is not yet over
+  displayNextTurn(getGameStatus(gameBoard)); // display board status
 }
 
 function displayNextTurn(gameStatus) {
-  // if ([false, "T", "X", "O"].indexOf(gameStatus) === -1) {
-  //   // if gameStatus is an invalid value...something other than null, T, X, or O
-  //   console.log("gameStatus ERROR!");
-  //   return;
-  // }
+  outputMsgEl.textContent = !gameStatus
+    ? `It's now ${isXsTurn(gameBoard) ? "X" : "O"}'s turn.` // display next turn when game's not over
+    : gameStatus === "T"
+    ? "Game Over - It's a tie!" // display a tie as needed
+    : `Game Over - ${gameStatus} Wins!`; // display the winner when the game is over and it's not a tie.
 
-  if (!gameStatus) {
-    // the game is not yet over
-    displayOutput(`It's now ${isXsTurn(gameBoard) ? "X" : "O"}'s turn.`);
-  } else if (gameStatus === "T") {
-    displayOutput("Game Over - It's a tie!");
-  } else {
-    // gameStatus must be X or O
-    displayOutput(`Game Over - ${gameStatus} Wins!`);
-  }
-
-  isGameOver = gameStatus ? true : false;
-}
-
-function displayOutput(text) {
-  outputMsgEl.textContent = text;
-
-  isGameOver = gameStatus ? true : false;
-}
-
-function displayOutput(text) {
-  document.getElementById("output-msg").textContent = text;
+  isGameOver = gameStatus !== false;
 }
 
 // Returns X, O, T, or False based on winner or not over
 function getGameStatus(board) {
-  // use gameBoard to determine the current status
-
-  // check for a vertical winner
-  for (let col = 0; col < 3; col++) {
-    if (!(board[0][col] === 0)) {
-      // no possible winner if it's empty!
-      if (board[0][col] === board[1][col] && board[0][col] === board[2][col]) {
-        return board[0][col]; // they won!
-      }
-    }
+  // if a winning combination is found, return which side won.
+  let resultsArray;
+  for (combo of winningCombinations) {
+    resultsArray = combo.map(([row, col]) => board[row][col]);
+    if (resultsArray.every((val) => val && val === resultsArray[0]))
+      return resultsArray[0];
   }
 
-  // check for a horizontal winner
-  for (let row = 0; row < 3; row++) {
-    if (!(board[row][0] === 0)) {
-      // no possible winner if it's empty!
-      if (board[row][0] === board[row][1] && board[row][0] === board[row][2]) {
-        return board[row][0]; // they won!
-      }
-    }
-  }
-
-  // check for diagonal winners
-  if (!(board[1][1] === 0)) {
-    // if the middle is empty, no diagonal win is possible
-    if (
-      (board[1][1] === board[0][0] && board[1][1] === board[2][2]) ||
-      // above checks the first diagonal
-      (board[1][1] === board[0][2] && board[1][1] === board[2][0])
-    ) {
-      // above checks the second diagonal -- if either were successful...
-      return board[1][1]; // they won!
-    }
-  } else {
-    return false; // can skip next part, as we know there is a move left!
-  }
-
-  // if all moves have been made it's a tie, otherwise return false
-  for (let row = 0; row < 3; row++) {
-    for (let col = 0; col < 3; col++) {
-      if (board[row][col] === 0) {
-        return false; // no result yet!
+  // if all moves have been made it's a tie, otherwise return false as not over
+  for (row of board) {
+    for (cell of row) {
+      if (!cell) {
+        return false; // at least one move remains, it's not a tie yet.
       }
     }
   }
@@ -290,8 +221,7 @@ function playComputerMove() {
 
     let whoseTurn = isXsTurn(myBoard) ? xM : oM;
 
-    // create a result array for the results of all possible moves
-    let myResults = [];
+    let myResults = []; // create a result array for the results of all possible moves
 
     // check all possible moves
     for (let row = 0; row < 3; row++) {
@@ -330,8 +260,8 @@ function playComputerMove() {
     otherSide = mySide === xM ? oM : xM;
 
     // cycle through all the results and select the best
-    for (row = 0; row < 3; row++) {
-      for (col = 0; col < 3; col++) {
+    for (row in resultArray) {
+      for (col in resultArray[row]) {
         switch (resultArray[row][col]) {
           case 0:
             break; // move already played
--- assets/js/test.js
+++ assets/js/test.js
@@ -1,3 +1,39 @@
 document.getElementById("start-btn").addEventListener("click", () => {
-    console.log(document.querySelector('input[name="play-side"]:checked').value);
-})
+  console.log(document.querySelector('input[name="play-side"]:checked').value);
+});
+
+let winningCombinations = [];
+
+// vertical options:
+winningCombinations.push(
+  ...[0, 1, 2].map((col) => [
+    [0, col],
+    [1, col],
+    [2, col],
+  ])
+);
+
+//horizontal
+winningCombinations.push(
+  ...[0, 1, 2].map((row) => [
+    [row, 0],
+    [row, 1],
+    [row, 2],
+  ])
+);
+
+// diagonal
+winningCombinations.push(
+  [
+    [0, 0],
+    [1, 1],
+    [2, 2],
+  ],
+  [
+    [0, 2],
+    [1, 1],
+    [2, 0],
+  ]
+);
+
+console.log(winningCombinations);
